d1ca150fcfe2f02f00dc024ea1ba6d5bcd96bec3
Felipe <reyes7.fr@gmail.com>
2023-05-12 12:37:47 -0400

    Cambios

--- PawCare/applications/home/urls.py
+++ PawCare/applications/home/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "home_app"
 
 urlpatterns = [
-    path('home/',views.HomeView.as_view(),name='home',),
+    path('',views.HomeView.as_view(),name='home',),
     path('colaboradores/',views.ColaboradoresView.as_view(),name='colaboradores',),
     path('servicios/',views.ServicioView.as_view(),name='servicios',),
     path('somos/',views.SomosView.as_view(),name='somos',),
